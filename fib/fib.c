f05187861f582616f66581b10ac12add5e7faf86
Serene Leyba <development@softwavestudios.com>
2024-02-25 23:18:31 -0700

    Updated fib.c
    Updated fib.c with contributions from Francesco146 and LizzyFleckenstein03 on gist

--- fib/fib.c
+++ fib/fib.c
@@ -1,58 +1,97 @@
-// Calculate Fibonacci Numbers 
+// Calculate Fibonacci Numbers
+// Originally written by Softwave (https://github.com/Softwave)
+// Massive speedups by Francesco146 and LizzyFleckenstein03 
+// (https://github.com/LizzyFleckenstein03) (https://github.com/Francesco146)
+// Public Domain
+// https://creativecommons.org/publicdomain/zero/1.0/
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <ctype.h>
 #include <gmp.h>
 #include <time.h>
 
-long limit, i = 0;
-
 int main(int argc, char *argv[])
 {
-	// Get User Input 
-	if (argc != 2)
-	{
-		printf("Improper input. Exiting.\n");
-		return -1; 
+	// Get User Input
+	if (argc != 2) {
+		printf("usage: %s NUM\n", argv[0]);
+		return EXIT_FAILURE;
 	}
 
-	limit = strtol(argv[1], NULL, 10);
+	long count = strtol(argv[1], NULL, 10);
 
-	// Setup GMP 
-	mpz_t a, b, c;
+	// Setup GMP
+	mpz_t a, b, p, q;
 	mpz_init_set_ui(a, 1);
-    mpz_init_set_ui(b, 0);
-   	mpz_init(c);
+	mpz_init_set_ui(b, 0);
+	mpz_init_set_ui(p, 0);
+	mpz_init_set_ui(q, 1);
 
-    // Start timing 
-    clock_t start_time = clock();
+	mpz_t tmp;
+	mpz_init(tmp);
 
-   	for (i = 0; i < limit; i++)
+    // Start timing
+    const clock_t start_time = clock();
+    if (start_time == (clock_t) {-1}) 
+    {
+        fprintf(stderr, "Error start_time clock()\n");
+        return EXIT_FAILURE;
+    }
+
+
+
+   	while (count > 0) 
    	{
-   		// Perform the Fibonacci Calculation
-   		mpz_add(c, a, b);
-   		mpz_set(a, b);
-   		mpz_set(b, c);
+		if (count % 2 == 0) 
+		{
+			mpz_mul(tmp, q, q);
+			mpz_mul(q, q, p);
+			mpz_mul_2exp(q, q, 1);
+			mpz_add(q, q, tmp);
+
+			mpz_mul(p, p, p);
+			mpz_add(p, p, tmp);
+
+			count /= 2;
+		} 
+		else 
+		{
+			mpz_mul(tmp, a, q);
+
+			mpz_mul(a, a, p);
+			mpz_addmul(a, b, q);
+			mpz_add(a, a, tmp);
+
+			mpz_mul(b, b, p);
+			mpz_add(b, b, tmp);
+
+			count -= 1;
+		}
    	}
 
-    // End timing 
-    clock_t end_time = clock(); 
+    // End timing
+    const clock_t end_time = clock();
+    if (end_time == (clock_t) {-1})
+    {
+        fprintf(stderr, "Error end_time clock()\n");
+        return EXIT_FAILURE;
+    }
 
-	// Print the results to stdout
-   	printf("Fibonacci Number %ld: ", i);
+
+    // Print the results to standard out
    	mpz_out_str(stdout, 10, b);
    	printf("\n");
 
 	// Cleanup
    	mpz_clear(a);
    	mpz_clear(b);
-   	mpz_clear(c);
+   	mpz_clear(p);
+   	mpz_clear(q);
+   	mpz_clear(tmp);
 
-    // Print time taken 
-    double time_taken = ((double) end_time - start_time)  / CLOCKS_PER_SEC;
-    printf("Calculation Time: %f seconds\n", time_taken);
-	
-	
-	return 0;
+    // Print time taken
+    const double time_taken = ((double) (end_time - start_time)) / (double) CLOCKS_PER_SEC;
+    if (printf("Calculation Time: %lf seconds\n", time_taken) < 0) return EXIT_FAILURE;
+    if (fflush(stdout) == EOF) return EXIT_FAILURE;
+    return EXIT_SUCCESS;
 }
